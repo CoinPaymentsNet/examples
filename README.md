78f5f4dde06b0f4fa558ae8a377c38119a1561a4
Dmytro Dmytrashchuk <dmytro@pay.nl>
2024-06-20 13:10:38 +0200

    Refactored code. Added composer, README.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Examples
+CoinPayments API v2 PHP Examples
+==========
+
+Provides set of PHP samples which will show how to interact with CoinPayments API v2. 
+
+## Installation
+
+Installation via [Composer](https://getcomposer.org/):
+
+```bash
+$ composer require coinpaymentscom/examples
+```
+
+or add it by hand to your `composer.json` file.
+
+You also can clone this repository and start using it directly (please don't forget to run `composer install` after cloning).
+
+## Usage
+
+Before starting, please rename `.env.dist` to `.env` and populate it with your `Client ID` and `Client Secret` obtained from CoinPayments BO.
+
+
+All examples can be executed from console:
+
+For instance:
+```bash
+$ php _PATH_TO_THIS_LIBRARY/src/examples/create_invoice.php
+```
---
+++ src/Helpers/ApiHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CoinPaymentsApiExamples\Helpers;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+
+class ApiHelper
+{
+    const API_URL = 'https://api.coinpayments.com';
+
+    /** @var string */
+    private $clientId;
+
+    /** @var string */
+    private $clientSecret;
+
+    public function __construct(string $clientId, string $clientSecret)
+    {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+    }
+
+    public function sendApiRequest(string $method, string $endpoint, array $payload = []): array
+    {
+        $payload = !empty($payload) ? json_encode($payload) : '';
+        $apiUrl  = sprintf('%s%s', self::API_URL, $endpoint);
+        $guzzle  = new Client(['verify' => false]);
+        try {
+            $response = $guzzle->request($method, $apiUrl, [
+                'body'    => $payload,
+                'headers' => $this->buildHeaders($method, $apiUrl, $payload),
+                'http_errors' => false,
+            ]);
+
+            $body = $response->getBody()->getContents();
+
+            return json_decode($body, true);
+        } catch (BadResponseException $e) {
+            return ['error' => $e->getResponse()];
+        }
+    }
+
+    private function buildHeaders(string $method, string $url, string $payload): array
+    {
+        $date            = (new \DateTimeImmutable())->setTimezone(new \DateTimeZone('UTC'))->format('Y-m-d\TH:i:s');
+        $signatureString = implode('', [chr(239), chr(187), chr(191), $method, $url, $this->clientId, $date, $payload]);
+
+        return [
+            'Content-Type'             => 'application/json',
+            'X-CoinPayments-Client'    => $this->clientId,
+            'X-CoinPayments-Timestamp' => $date,
+            'X-CoinPayments-Signature' => base64_encode(hash_hmac('sha256', $signatureString, $this->clientSecret, true)),
+        ];
+
+    }
+}
