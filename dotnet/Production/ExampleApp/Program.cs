07bd38df3b4dfa5f4fc80ce0fd52ab2b802a41c5
furkan.yucel <furkan.yucel@hypedriven.com>
2024-11-25 23:13:17 +0300

    hotfix

--- dotnet/Production/ExampleApp/Program.cs
+++ dotnet/Production/ExampleApp/Program.cs
@@ -5,9 +5,6 @@ using Shared.Models;
 
 
 var prodClient = new ProdApiClient("https://api.coinpayments.com/api/v1");
-var invoiceService = new InvoiceService(prodClient);
-
-// await invoiceService.CreateInvoiceInUSD_and_List_LTCT_Payouts(20_00);//Must be specified in cents 20_00=> $20
 
 var rateService = new RateService(prodClient);
 var usdAmount = 30_00;//Must be specified in cents 20_00=> $20
