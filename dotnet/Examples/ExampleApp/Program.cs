e876c33963fb9bcda8995eb89e63172a920b8ecf
WheatSlice <trevor.fannin@hotmail.com>
2025-05-20 18:26:37 -0500

    fix: urls

--- dotnet/Examples/ExampleApp/Program.cs
+++ dotnet/Examples/ExampleApp/Program.cs
@@ -5,9 +5,9 @@ using Shared;
 
 var currentClient =new ClientEnvironmentModel()
 {
-    Id= "bb1194edff814fabafb567c12e43df66",
-    Secret= "SCrYiYalS6XvksCfidIOO1YgAnFu+ht1MrxVnSHmUmY="
+    Id= "",
+    Secret= ""
 };
-var client = new CoinPaymentsApiClient("https://api.coinpayments.com/api/v1", currentClient);
+var client = new CoinPaymentsApiClient("https://api.coinpayments.net/api/v1", currentClient);
 var exampleCreator = new ExampleCreator(client);
 await exampleCreator.CreateInvoiceInUSD();
